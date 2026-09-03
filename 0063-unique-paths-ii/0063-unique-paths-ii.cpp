ff20b30ac02fb09dbc02ff46d9147919ee5cb1dc
Akhil Singh <akhilsingh8113@gmail.com>
2026-09-03 15:01:57 +0530

    Time: 0 ms (100%), Space: 11.9 MB (31.57%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -22,6 +22,26 @@ public:
         if( obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]) return 0;
 
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return help(obstacleGrid,m-1,n-1,dp);
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+               if(i==0 && j==0){
+                dp[i][j]=1;
+                continue;
+               }
+                int right=0;
+                int down=0;
+                if(obstacleGrid[i][j]!=1){
+                    if(i>0)
+                        right=dp[i-1][j];
+
+                    if(j>0)
+                        down=dp[i][j-1];
+                }
+                dp[i][j]=right+down;
+
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
