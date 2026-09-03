9e8c2a40ad344f854dc171c2f44a44b06aa42ff0
Akhil Singh <akhilsingh8113@gmail.com>
2026-09-03 23:44:57 +0530

    Time: 4 ms (15.04%), Space: 16.4 MB (16.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -21,6 +21,25 @@ public:
         int n=grid[0].size();
 
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return help(grid,m-1,n-1,dp); 
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=grid[i][j];
+                    continue;
+                }
+
+                int up =1e9;
+                if(i>0)
+                    up=grid[i][j]+ dp[i-1][j];
+
+                int left = 1e9;
+                if(j>0)
+                    left=grid[i][j]+dp[i][j-1];
+
+                dp[i][j]=min(up,left);
+            }
+        }
+        return dp[m-1][n-1]; 
     }
 };
